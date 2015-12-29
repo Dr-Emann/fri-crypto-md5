19c2e05317d36e2eb9bf08539c6df578c056fc6e
Zachary Dremann <dremann@gmail.com>
2015-12-29 01:07:40 -0500

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -6,93 +6,80 @@ use crypto::sha2::Sha256;
 use rand::Rng;
 
 use std::fmt::Write;
-use std::char;
+use std::fmt;
 use std::collections::HashMap;
+use std::collections::hash_map::Entry::*;
 
-fn hexprint(raw: &[u8; 6]) {
-    let mut s = String::new();
-    for &byte in raw.iter() {
-        write!(&mut s, "{:02X}", byte).unwrap();
+struct HexDisplay<'a>(&'a [u8]);
+
+impl<'a> fmt::Display for HexDisplay<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for i in self.0 {
+            try!(write!(f, "{:02X}", i));
+        }
+        Ok(())
     }
-    println!("{}", s);
 }
 
-fn truncate(raw: &[u8]) -> [u8; 6] {
-    [raw[26], raw[27], raw[28], raw[29], raw[30], raw[31]]
+fn truncated_hash(data: &str) -> [u8; 6] {
+    let full_hash = oneshot_hash(data);
+    [full_hash[26], full_hash[27], full_hash[28], full_hash[29], full_hash[30], full_hash[31]]
 }
 
-// Returns semirandom a-zA-Z0-9 from byte
-fn to_valid_char(b: u8) -> char {
-	let b_32 = (b % 62) as u32;
-	match b_32 {
-		0  ... 25 => char::from_u32(b_32 + 65).unwrap(),
-		26 ... 51 => char::from_u32(b_32 - 26 + 97).unwrap(),
-		_         => char::from_u32(b_32 - 52 + 48).unwrap()
-	}
+fn oneshot_hash(data: &str) -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    hasher.input_str(data);
+    let mut full_hash = [0u8; 32]; // could use mem::uninitialized
+    hasher.result(&mut full_hash);
+    full_hash
 }
+
 
 fn main() {
-    let mut bigtable = HashMap::<[u8; 6], usize>::new();    
-    let mut rng = rand::thread_rng();
-    let mut hasher = Sha256::new();
+    let mut bigtable = HashMap::<[u8; 6], usize>::new();
+    let mut rng = rand::weak_rng();
     
-    let len = 8;
-	let mut bigstring : String = (0..len)
-            .map(|_| to_valid_char(rng.gen::<u8>() % 62))
-            .collect::<String>();
-     
-    let mut i = 0 as usize;
-    loop {
-		// Need a block to end borrow of bigstring
-		let thex = 
-		{
-			let hstr = &bigstring[i .. i+len];
-			hasher.input_str(hstr);
+    const LEN: usize = 8;
 
-			// read hash digest
-			let mut hex = [0u8; 32];
-			hasher.result(&mut hex);
-			hasher.reset();
-			
-			// truncate
-			let thex = truncate(&hex);
-			
-			if bigtable.contains_key(&thex)
-			{
-				let idx = bigtable[&thex];
-				let str1 = &bigstring[idx .. idx+len];
-				let str2 = hstr;
-				
-				if str1 != str2 {
-					hasher.input_str(str1);
-					let mut h1 = [0u8; 32];
-					hasher.result(&mut h1);
-					hasher.reset();
-					
-					hasher.input_str(str2);
-					let mut h2 = [0u8; 32];
-					hasher.result(&mut h2);
-					hasher.reset();
-					
-					if h1[25] & 0x3 == h2[25] & 0x3	{
-						println!("\t-> {:08}", i);
-						print!("EVO: {} .. {} -> ", &str1, &str2);				
-						hexprint(&thex);
-						break;
-					} else {
-						print!("?");
-					}
-				} else {
-					print!("*");
-				}
-			}
-			thex
-		};
+    let mut bigstring = rng.gen_ascii_chars()
+        .take(LEN)
+        .collect::<String>();
+     
+    for i in 0usize.. {
+        // Need a block to end borrow of bigstring
+        {
+            let hstr = &bigstring[i .. i+LEN];
+            let thex = truncated_hash(hstr);
+            
+            match bigtable.entry(thex) {
+                Occupied(mut entry) => {
+                    let old_idx = entry.insert(i);
+                    let old_str = &bigstring[old_idx..old_idx+LEN];
+                    let new_str = hstr;
+                    if new_str != old_str {
+                        let old_hash = oneshot_hash(old_str);
+                        let new_hash = oneshot_hash(hstr);
+                        if old_hash[25] & 0x3 == new_hash[25] & 0x3 {
+                            println!("\t-> {:08}", i);
+                            println!("EVO: {} .. {} -> {}", old_str, new_str, HexDisplay(&thex));
+                            break;
+                        }
+                        else {
+                            println!("?");
+                        }
+                    }
+                    else {
+                        print!("*");
+                    }
+                },
+                Vacant(entry) => {
+                    entry.insert(i);
+                },
+            }
+        }
         
-        bigtable.insert(thex, i);
-        bigstring.push(to_valid_char(rng.gen::<u8>()));
-		
-        i += 1;
+        bigstring.push(rng.gen_ascii_chars().next().unwrap());
+        
         if i % 2000000 == 0 {
             println!("\t-> {:08}", i);
         }
